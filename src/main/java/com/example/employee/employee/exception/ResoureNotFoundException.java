0de4157fbf6035026ab06bc0fc4d184bba17553b
nhannguyendn <nhannguyedn.nta@gmail.com>
2025-08-14 13:45:51 +0700

    Add project

---
+++ src/main/java/com/example/employee/employee/exception/ResoureNotFoundException.java
@@ -0,0 +1,13 @@
+package com.example.employee.employee.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus( value = HttpStatus.NOT_FOUND)
+public class ResoureNotFoundException extends RuntimeException {
+
+    public ResoureNotFoundException(String messsage) {
+        super(messsage);
+    }
+
+}
