d95d8542cfecfba00b0d4cb9fb4e1743d8fe2bac
nhannguyendn <nhannguyedn.nta@gmail.com>
2025-08-19 09:04:18 +0700

    Add gradle config

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("org.springframework.boot") version "3.0.4"
+    id("io.spring.dependency-management") version "1.1.0"
+    kotlin("jvm") version "1.8.0" apply false
+    java
+}
+
+group = "com.example.employee"
+version = "0.0.1-SNAPSHOT"
+description = "Demo project for Spring Boot"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    runtimeOnly("com.mysql:mysql-connector-j")
+
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
