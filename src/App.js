5061dc92548c5615d90b9ac5a5e9f8bdff197261
nhannguyendn <nhannguyedn.nta@gmail.com>
2025-08-19 11:03:29 +0700

    Add react router dom

--- src/App.js
+++ src/App.js
@@ -1,59 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ListEmployeeComponent from './components/ListEmployeeComponent';
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
 
 function App() {
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <h1>Hello word!</h1>
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
 
   return (
     <div className="App">
-      <div>
-        <HeaderComponent />
-      </div>
+      <Router>
+        <div>
+          <HeaderComponent />
+        </div>
 
-      <header className="App-header">
-        <h1>Hello word!</h1>
-        <img src={logo} className="App-logo" alt="logo" width={100} height={100} style={{ marginTop: '0px', marginBottom: '10px' }} />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="App-header">
+          <h1>Hello word!</h1>
+          <img src={logo} className="App-logo" alt="logo" width={100} height={100} style={{ marginTop: '0px', marginBottom: '10px' }} />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
 
-      <main className='container'>
-        <ListEmployeeComponent />
-      </main>
+        <main className='container'>
 
-      <div>
-        <FooterComponent className="bg-dark text-white text-center p-2"/>
-      </div>
+          <div>
+            <Routes>
+              <Route path="/" element={<ListEmployeeComponent />}></Route>
+              <Route path="/employees" element={<ListEmployeeComponent />}></Route>
+            </Routes>
+          </div>
+        </main>
+
+         <FooterComponent className="bg-dark text-white text-center p-2" />
+      </Router>
     </div>
   )
 }
