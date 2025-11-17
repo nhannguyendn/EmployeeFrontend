8eee23a3e0b664151703eaead96f63dd2a43caa0
nhannv <nhannv@mail.nitrotech.asia>
2025-11-17 14:25:10 +0700

    - update config dev, staging docker, production aws

--- src/config.js
+++ src/config.js
@@ -1,12 +1,9 @@
-const ENV = process.env.NODE_ENV;
+const STAGE = process.env.REACT_APP_STAGE;
 
 const CONFIG = {
-  development: {
-    API_BASE_URL: "http://localhost:8888/api/v1",
-  },
-  production: {
-    API_BASE_URL: "http://springbootdemo.ap-southeast-2.elasticbeanstalk.com/api/v1",
-  },
+  development: { API_BASE_URL: "http://localhost:8888/api/v1" },
+  staging: { API_BASE_URL: "http://localhost:9090/api/v1" },
+  production: { API_BASE_URL: "http://springbootdemo.ap-southeast-2.elasticbeanstalk.com/api/v1" },
 };
 
-export const API_BASE_URL = CONFIG[ENV].API_BASE_URL;
+export const API_BASE_URL = CONFIG[STAGE].API_BASE_URL;
